d073f44574eded2170bc125eb2edb80404ed7b13
854581202 <854581202@qq.com>
2019-10-27 01:33:28 -0700

    learn python3 2019-10-27

---
+++ errorDemo.py
@@ -0,0 +1,32 @@
+# -*- coding:utf-8 -*-
+from functools import reduce
+
+#这是后加上去的，为了防止str2num处理“7.6”时报错。
+def log(func):
+    def wraper(*args,**kw):
+        s = args[0]
+        if "." in s:
+            a,b=s.split(".")
+            c=int(a)+int(b)/10**len(b)
+            return func(c)
+        else:
+            return func(*args,**kw)
+    return wraper
+
+@log
+def str2num(s):
+    return int(s)
+
+def calc(exp):
+    ss = exp.split('+')
+    ns = map(str2num, ss)
+    return reduce(lambda acc, x: acc + x, ns)
+
+def main():
+    r = calc('100 + 200 + 345')
+    print('100 + 200 + 345 =', r)
+    r = calc('99 + 88 + 7.6')
+    print('99 + 88 + 7.6 =', r)
+
+
+main()
